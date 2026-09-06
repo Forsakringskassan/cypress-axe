b38075b7e95f6524b84ce84f203cd0eb058474d5
David Sveningsson <ext@sidvind.com>
2026-09-06 10:39:39 +0200

    chore(deps): update dependency cypress to v16

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,6 @@ import { init as installAxe } from "@forsakringskassan/cypress-axe/plugins";
 const PORT: number = Number(process.env.FK_AXE_SERVER_PORT ?? "8080");
 
 export default defineConfig({
-    allowCypressEnv: false,
-
     e2e: {
         baseUrl: `http://localhost:${PORT}`,
         setupNodeEvents(on, config) {
